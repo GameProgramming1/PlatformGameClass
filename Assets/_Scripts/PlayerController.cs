73535a812a73d9e673b9b0226cbc5f5bbc5a3a51
Sukhpreet Sekhon <ersukhpreetsekhon03@gmail.com>
2015-10-22 12:07:02 -0400

    added scripts and sound

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //private instance variable
+    private AudioSource[] _audioSources;
+    private AudioSource _CoinSound;
+
+	// Use this for initialization
+	void Start () {
+        this._audioSources = gameObject.GetComponents<AudioSource>();
+        this._CoinSound =this._audioSources[0];
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D otherCollider)
+    {
+        if (otherCollider.gameObject.CompareTag("Coin"))
+        {
+            this._CoinSound.Play();
+        }
+
+    }
+}
