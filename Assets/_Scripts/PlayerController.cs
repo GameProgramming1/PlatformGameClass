2a4b48670d2344ca8f4e5021954eb8b1165fdefe
Sukhpreet Sekhon <ersukhpreetsekhon03@gmail.com>
2015-10-23 12:00:51 -0400

    added player jump script and jump sound

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -28,8 +28,11 @@ public class PlayerController : MonoBehaviour {
     private Animator _animator; //will use it later
     private AudioSource[] _audioSources;
     private AudioSource _CoinSound;
+    private AudioSource _JumpSound;
 
-    private float _moving = 0;
+    private float _movingValue = 0;
+    private bool _isFlippingRight = true;
+    private bool _isGrounded = true;
 
 	// Use this for initialization
 	void Start () {
@@ -40,6 +43,7 @@ public class PlayerController : MonoBehaviour {
 
         this._audioSources = gameObject.GetComponents<AudioSource>();
         this._CoinSound =this._audioSources[0];
+        this._JumpSound = this._audioSources[1];
 	
 	}
 	
@@ -52,41 +56,73 @@ public class PlayerController : MonoBehaviour {
         float absVelX = Mathf.Abs(this._rigidBody2D.velocity.x);
         float absVelY = Mathf.Abs(this._rigidBody2D.velocity.y);
 
-        this._moving = Input.GetAxis("Horizontal");
+        this._movingValue = Input.GetAxis("Horizontal");
 
-        if(this._moving != 0)
+        if(this._movingValue != 0)
         {
             //we are moving
 
             this._animator.SetInteger("AnimState", 1);//walk
-            if(this._moving>0)
+            if(this._movingValue>0)
             {
             //we are moving right
                 if(absVelX < this.velocityRange.vMax)
                 {
                     forceX = this.speed;
+                    this._isFlippingRight = true;
+                    this.flip();
                 }
             }
-            else if (this._moving<0)
+            else if (this._movingValue<0)
             {
             //we are moving left
                 if (absVelX < this.velocityRange.vMax)
                 {
                     forceX = - this.speed;
+                    this._isFlippingRight = false;
+                    this.flip();
                 }
             }
         }
 
-        else if (this._moving == 0) 
+        else if (this._movingValue == 0) 
         {
         //we are not moving
             this._animator.SetInteger("AnimState", 0);
         }
 
+        //to check if player is jumping
+        if((Input.GetKey("up") || Input.GetKey(KeyCode.W)))
+        { //player is jumping
+            
+            if (this._isGrounded)
+            {
+               
+                if (absVelY < this.velocityRange.vMax)
+                {
+                    forceY = this.jump;
+                    this._animator.SetInteger("Animstate", 2);
+                    this._JumpSound.Play();
+                    this._isGrounded = false;
+                }
+            }
+        
+        }
+      
+
         this._rigidBody2D.AddForce(new Vector2(forceX, forceY));
 	
 	}
+    // on collision with platform
+    void OnCollisionStay2D(Collision2D otherCollider)
+    {
+        if (otherCollider.gameObject.CompareTag("Platform"))
+        {
+            this._isGrounded = true;
+        }
+    }
 
+    //on collision with coin
     void OnCollisionEnter2D(Collision2D otherCollider)
     {
         if (otherCollider.gameObject.CompareTag("Coin"))
@@ -94,5 +130,17 @@ public class PlayerController : MonoBehaviour {
             this._CoinSound.Play();
         }
 
+    }
+
+    private void flip()
+    { 
+    if(this._isFlippingRight)
+    {
+        this._transform.localScale = new Vector2(1f,1f);
+    }
+    else 
+    {
+        this._transform.localScale = new Vector2(-1f, 1f);
+    }
     }
 }
